8bf3b684ae9cfec3cfcad92f3ed7298c28a9d75a
Nathan <nathannxx@proton.me>
2026-03-04 14:34:15 +0700

    fixup! tanzanite: Switch to source-built C2 service
    Signed-off-by: Nathan <nathannxx@proton.me>

--- device.mk
+++ device.mk
@@ -189,6 +189,7 @@ PRODUCT_PACKAGES += \
 # Media (C2)
 $(call soong_config_set_bool,android_hardware_mediatek_codec2,link_v33_libstagefright_foundation,true)
 PRODUCT_PACKAGES += \
+    android.hardware.media.c2-mtk-service \
     libcodec2_soft_common.vendor:64 \
     libeffects:64 \
     libeffectsconfig.vendor:64
